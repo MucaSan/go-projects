f9a0a048bc0f445d8066513e0bf83b6d50d38006
MucaSan <villar.mucasan@gmail.com>
2024-08-10 23:33:33 -0300

    push new learned stuff

---
+++ learn/hello-world/hello.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+// first program written in golang, let's dive into the adventure!
+func main() {
+	fmt.Println("Hello world!")
+}
--- learn/variables/variables.go
+++ learn/variables/variables.go
@@ -1,11 +1,12 @@
-package variables
+package main
 
 import "fmt"
 
 func main() {
+	// manually set the value, based on the number inserted.
 	var number1 int = 32
+	// automatically instantiates the type of the variable, based on the value the programmer has inputed.
 	number2 := 32
-
 	fmt.Println(number1)
 	fmt.Println(number2)
 
